70c9a99b1ab11bfac4c03eb15e86be6e1ae97631
Stepan Turchenko <56656842+interhub@users.noreply.github.com>
2019-11-30 05:12:36 +0200

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+document.body.style.backgroundColor = "#4649ad";
+document.body.style.color = "#8afff6";
+
+function go() {
+    document.getElementById('reg').innerHTML = "Спасибо за регистрацию";
+
+}
